def10a5fcc19e519072c4c7737490d8b09f18b9f
shumaila imran <shumimran623@gmail.com>
2024-05-23 11:06:20 +0500

    personal message

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Name Cases: Store a person’s name in a variable, and then print that 
+// person’s name in lowercase, uppercase, and titlecase.
+let person_name = "shumaila imran";
+console.log("uppercase", person_name.toUpperCase());
+console.log("lowercase", person_name.toLowerCase());
+console.log("titlecase:", person_name.replace(/\b\w/g, (per) => per.toUpperCase()));
